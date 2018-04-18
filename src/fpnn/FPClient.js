f52f0d6d909be2d8ec7c9648856fa9b2b141b8fb
zhao.dx <zhaod1025@gmail.com>
2018-04-18 15:39:53 +0800

    fix

--- src/fpnn/FPClient.js
+++ src/fpnn/FPClient.js
@@ -193,8 +193,7 @@ function onClose(){
 
 function reConnect(){
     if (this._intervalID){
-        clearInterval(this._intervalID);
-        this._intervalID = 0;
+        return;
     }
 
     let self = this;
